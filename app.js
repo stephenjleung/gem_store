7ea15e21bf20a8c68d8d8716897867cfa47bd5ef
Stephen Leung <stephenjleung@gmail.com>
2016-12-17 16:02:11 -0800

    Add more products. Add repeat and show/hide directives.

--- app.js
+++ app.js
@@ -3,14 +3,33 @@
 
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
+  var gems = [
+  {
     name: '1 Mythril',
     price: 0.25,
-    description: 'One Mythril'
+    description: 'One Mythril',
+    canPurchase: true,
+    soldOut: false
+  },
+  {
+    name: '5 Mythril',
+    price: 1.00,
+    description: 'Five Mythril',
+    canPurchase: true,
+    soldOut: false
+  },
+  {
+    name: '15 Mythril',
+    price: 2.50,
+    description: 'Fifteen Mythril',
+    canPurchase: true,
+    soldOut: false
   }
+
+  ];
 
 })();
 
