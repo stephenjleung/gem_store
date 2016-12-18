989d073fc346f1f6d248004e68adf43d2fa78be9
Stephen Leung <stephenjleung@gmail.com>
2016-12-17 17:21:21 -0800

    Add full and thumb images. Repeat based on mythrilCount. Style img gallery

--- app.js
+++ app.js
@@ -2,30 +2,39 @@
   var app = angular.module('store', []);  
 
 
-  app.controller('StoreController', function(){
+  app.controller('StoreController', function($scope){
+
+    $scope.getNumber = function(num) {
+      return new Array(num);
+    }
+
     this.products = gems;
   });
 
   var gems = [
   {
     name: '5 Mythril',
+    mythrilCount: 5,
     price: 1.00,
     description: 'Five Mythril',
     images: [
       {
-        full:'img/1mythril.png'
+        full:'img/1mythril-full.png',
+        thumb: 'img/1mythril-thumb.png'
       }
     ],
     canPurchase: true,
-    soldOut: false
+    soldOut: false    
   },
   {
     name: '15 Mythril',
+    mythrilCount: 15,
     price: 2.50,
     description: 'Fifteen Mythril',
     images: [
       {
-        full:'img/1mythril.png'
+        full:'img/1mythril-full.png',
+        thumb: 'img/1mythril-thumb.png'
       }
     ],
     canPurchase: true,
@@ -33,27 +42,43 @@
   },
   {
     name: '25 Mythril',
+    mythrilCount: 25,
     price: 3.50,
     description: 'Twenty Five Mythril',
     images: [
       {
-        full:'img/1mythril.png'
+        full:'img/1mythril-full.png',
+        thumb: 'img/1mythril-thumb.png'
       }
     ],
     canPurchase: true,
-    soldOut: false
+    soldOut: false    
   },
   {
     name: '50 Mythril',
+    mythrilCount: 50,
     price: 6.00,
     description: 'Fifty Mythril',
     images: [
       {
-        full:'img/1mythril.png'
+        full:'img/1mythril-full.png',
+        thumb: 'img/1mythril-thumb.png'
       }
     ],
     canPurchase: true,
+    soldOut: false    
+  },
+  {
+    name: '100 Mythril',
+    mythrilCount: 100,
+    price: 10.00,
+    description: 'One Hundred Mythril',
+    images: [
+      
+    ],
+    canPurchase: true,
     soldOut: false
+
   }
 
   ];
