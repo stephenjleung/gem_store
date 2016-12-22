ceada012e288c0e3d8bb7d9e5dcd4ec3ef8d76fc
Stephen Leung <stephenjleung@gmail.com>
2016-12-21 16:22:51 -0800

    Add review controller for form submission.

--- app.js
+++ app.js
@@ -2,32 +2,45 @@
   var app = angular.module('store', []);  
 
 
-  app.controller('StoreController', function($scope){
+  app.controller('StoreController', function($scope) {
 
     // Build an array of size num for ng-repeat use
     $scope.getNumber = function(num) {
       return new Array(num);
-    }
+    };
 
     this.products = gems;
   });
 
-  app.controller('GalleryController', function(){
+  app.controller('GalleryController', function() {
     this.current = 0;
     this.setCurrent = function(newCurrent) {
       this.current = newCurrent || 0;
     };
   });
 
-  app.controller('PanelController', function(){
+  app.controller('PanelController', function() {
     this.tab = 1;
     this.selectTab = function(setTab) {
       this.tab = setTab;
-    }
+    };
     this.isSelected = function(checkTab) {
       return this.tab === checkTab;
-    }
+    };
   });
+
+  app.controller('ReviewController', function() {
+
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+
+
+  });
+
 
 
 
